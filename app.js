0aee6856809feebfd47f00ed9e2b0a2ba39c5de6
yanling-99 <love87090959@gmail.com>
2023-11-20 14:47:55 +0800

    [feat] let the snake to move

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const col = canvas.width / bodyUnit;
 
 let snake = []; // store objects
 // each object stores a (x,y) of rect.(snake's body)
-snake[0] = {
+snake[0] = { // head
     x: 80,
     y: 0
 };
@@ -28,16 +28,45 @@ snake[3] = {
     y: 0
 };
 
-// fill snake's body with colors
-for (let i = 0; i < snake.length; i++) {
-    if (i == 0) {
-        ctx.fillStyle = 'yellow';
-    } else {
-        ctx.fillStyle = 'lightblue';
-    }
-    ctx.strokeStyle = 'white'; // border style
-    // rendering with (x, y, width, height)
-    ctx.fillRect(snake[i].x, snake[i].y, bodyUnit, bodyUnit);
-    ctx.strokeRect(snake[i].x, snake[i].y, bodyUnit, bodyUnit);
-}
+let moveDirection = 'Right';
+let snakeMove = setInterval(draw, 100);
 
+function draw() {
+    // refresh canvas graph
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // fill/refill snake's body with colors
+    for (let i = 0; i < snake.length; i++) {
+        if (i == 0) {
+            ctx.fillStyle = 'yellow';
+        } else {
+            ctx.fillStyle = 'lightblue';
+        }
+        ctx.strokeStyle = 'white'; // border style
+        // rendering with (x, y, width, height)
+        ctx.fillRect(snake[i].x, snake[i].y, bodyUnit, bodyUnit);
+        ctx.strokeRect(snake[i].x, snake[i].y, bodyUnit, bodyUnit);
+    }
+
+    // decide the coordinates of next head unit depend on moveDirection's value
+    let nextSnakeX = snake[0].x;
+    let nextSnakeY = snake[0].y;
+    if (moveDirection == 'Left') {
+        nextSnakeX -= bodyUnit;
+    } else if (moveDirection == 'Up') {
+        nextSnakeY -= bodyUnit;
+    } else if (moveDirection == 'Right') {
+        nextSnakeX += bodyUnit;
+    } else if (moveDirection == 'Down') {
+        nextSnakeY += bodyUnit;
+    }
+
+    let nextHeadUnit = {
+        x: nextSnakeX,
+        y: nextSnakeY
+    };
+
+    snake.pop();
+    snake.unshift(nextHeadUnit);
+}
