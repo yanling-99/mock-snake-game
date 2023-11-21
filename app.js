6ed49b110e4b2b2c7d045843387bc6457b842639
yanling-99 <love87090959@gmail.com>
2023-11-21 11:08:18 +0800

    [feat] fruit/score setting

--- app.js
+++ app.js
@@ -28,6 +28,47 @@ snake[3] = {
     y: 0
 };
 
+class Fruit {
+    constructor() {
+        this.x = Math.floor(Math.random() * col) * bodyUnit;
+        this.y = Math.floor(Math.random() * row) * bodyUnit;
+    }
+
+    drawFruit() {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.x, this.y, bodyUnit, bodyUnit);
+    }
+
+    pickAnewPosition() {
+        let overlapping = false;
+        let newX;
+        let newY;
+
+        function checkOverlap(x, y) {
+            for (let i = 0; i < snake.length; i++) {
+                if (snake[i].x == x && snake[i].y == y) {
+                    overlapping = true;
+                    return;
+                } else {
+                    overlapping = false;
+                }
+            }
+        }
+
+        do {
+            newX = Math.floor(Math.random() * col) * bodyUnit;
+            newY = Math.floor(Math.random() * row) * bodyUnit;
+            checkOverlap(newX, newY);
+        }
+        while (overlapping);
+
+        this.x = newX;
+        this.y = newY;
+    }
+}
+
+let myFruit = new Fruit();
+
 // change moving direction by direction keys
 let moveDirection = 'Right';
 window.addEventListener('keydown', changeDirectionValue);
@@ -35,26 +76,13 @@ window.addEventListener('keydown', changeDirectionValue);
 // update snake's state every 0.1 second
 let snakeMove = setInterval(draw, 100);
 
-function changeDirectionValue(e) {
-    if (e.key == 'ArrowLeft' && moveDirection != 'Right') {
-        moveDirection = 'Left';
-    }
-    else if (e.key == 'ArrowRight' && moveDirection != 'Left') {
-        moveDirection = 'Right';
-    }
-    else if (e.key == 'ArrowUp' && moveDirection != 'Down') {
-        moveDirection = 'Up';
-    }
-    else if (e.key == 'ArrowDown' && moveDirection != 'Up') {
-        moveDirection = 'Down';
-    }
-}
 
 function draw() {
     // refresh canvas graph
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    myFruit.drawFruit();
     // fill/refill snake's body with colors
     fillColor();
 
@@ -62,7 +90,14 @@ function draw() {
     let nextHeadUnit = changeDirection();
 
     // confirm that whether the snake ate a point
-    snake.pop();
+    if (snake[0].x == myFruit.x && snake[0].y == myFruit.y) {
+        // new random position of the fruit
+        myFruit.pickAnewPosition();
+        // update the score
+
+    } else {
+        snake.pop();
+    }
     snake.unshift(nextHeadUnit);
 
 
@@ -111,4 +146,19 @@ function changeDirection() {
         nextSnakeY += bodyUnit;
     }
     return { x: nextSnakeX, y: nextSnakeY };
+}
+
+function changeDirectionValue(e) {
+    if (e.key == 'ArrowLeft' && moveDirection != 'Right') {
+        moveDirection = 'Left';
+    }
+    else if (e.key == 'ArrowRight' && moveDirection != 'Left') {
+        moveDirection = 'Right';
+    }
+    else if (e.key == 'ArrowUp' && moveDirection != 'Down') {
+        moveDirection = 'Up';
+    }
+    else if (e.key == 'ArrowDown' && moveDirection != 'Up') {
+        moveDirection = 'Down';
+    }
 }
